03a3f154b08de9971af3c990953513cbaf805ade
nopresent <nopresent@gmail.com>
2021-09-15 22:49:35 +0300

    done

--- src/lab/nopresent/Main.java
+++ src/lab/nopresent/Main.java
@@ -3,9 +3,25 @@ package lab.nopresent;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("2");
-        System.out.println("3");
 
+        // byte short
+        byte b1 = 13;
+        short s1 = 15_001;
+        System.out.println("       -> +: "+(b1+s1));
+        System.out.println("       -> *: "+(b1*s1));
+        
+        // short double float
+        short s2 = -17;
+        short s3 = 356;
+        double double_s_Result = s2/(double)s3;
+
+        System.out.println(" float -> /: "+(float)s2/s3);
+        System.out.println("double -> /: "+double_s_Result);
+
+        // long
+        long l1 = 2_000_000;
+        long l2 = 5_000;
+        System.out.println("       -> -: "+(l1-l2));
 
     }
 }
