ad9657e79d6679e47a4b5f5a8eef8bbad97e73bf
nopresent <nopresent@gmail.com>
2021-09-18 10:41:19 +0300

    code formatted

--- src/lab/nopresent/Main.java
+++ src/lab/nopresent/Main.java
@@ -7,21 +7,21 @@ public class Main {
         // byte short
         byte b1 = 13;
         short s1 = 15_001;
-        System.out.println("       -> +: "+(b1+s1));
-        System.out.println("       -> *: "+(b1*s1));
-        
+        System.out.println("       -> +: " + (b1 + s1));
+        System.out.println("       -> *: " + (b1 * s1));
+
         // short double float
         short s2 = -17;
         short s3 = 356;
-        double double_s_Result = s2/(double)s3;
+        double double_s_Result = s2 / (double) s3;
 
-        System.out.println(" float -> /: "+(float)s2/s3);
-        System.out.println("double -> /: "+double_s_Result);
+        System.out.println(" float -> /: " + (float) s2 / s3);
+        System.out.println("double -> /: " + double_s_Result);
 
         // long
         long l1 = 2_000_000L;
         long l2 = 5_000L;
-        System.out.println("       -> -: "+(l1-l2));
+        System.out.println("       -> -: " + (l1 - l2));
 
     }
 }
